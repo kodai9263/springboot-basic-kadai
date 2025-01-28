38ed79d7f0ce53b75d2bcae2c4f1be6ef264b884
kodai9263 <kou92638685@gmail.com>
2025-01-28 16:15:39 +0900

    お問い合わせフォームを作ろう

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,38 @@
+package com.example.springkadaiform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ContactFormController {
+
+	//入力画面表示
+    @GetMapping("/form")
+    public String input(Model model) {
+        model.addAttribute("contactForm", new ContactForm());
+        return "contactFormView";
+    }
+
+    //確認画面表示
+    @GetMapping("/confirm")
+    public String confirm(ContactForm form, Model model) {
+        model.addAttribute("contactForm", form);
+        return "confirmView";
+    }
+
+    @PostMapping("/form")
+    public String form(RedirectAttributes redirectAttributes, @Validated ContactForm form, BindingResult bindingResult) {
+		//バリデーションエラーがあったら終了
+        if (bindingResult.hasErrors()) {
+            return "contactFormView";
+        }
+		//バリデーションエラーがなかったら確認画面に移動
+        return "confirmView";
+    }
+
+}
